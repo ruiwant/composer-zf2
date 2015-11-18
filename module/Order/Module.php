78cfe1dbf47ee7b7eb0a3de726b1780da50f00f5
Ruiwant <ruiwant@qq.com>
2015-11-18 22:37:20 +0800

    test commit on github editor page
    test commit on github editor page

--- module/Order/Module.php
+++ module/Order/Module.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Zend Framework (http://framework.zend.com/)
- *
+ * test
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
